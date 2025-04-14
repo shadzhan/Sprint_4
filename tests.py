ad6a29d4e8532b0ecbd85c0271e1a01981b6a969
Albert Shadzhanov <albertm79@yandex.ru>
2025-04-14 21:54:08 +0300

    Adding existing book in favorites

--- tests.py
+++ tests.py
@@ -75,3 +75,8 @@ class TestBooksCollector:
         collector.set_book_genre('Ужасная история', 'Ужасы')
         assert collector.get_books_for_children() == []
 
+    def test_add_book_in_favorites_existing_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Голова профессора Доуля')
+        collector.add_book_in_favorites('Голова профессора Доуля')
+        assert collector.get_list_of_favorites_books() == ['Голова профессора Доуля']
