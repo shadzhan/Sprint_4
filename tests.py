406a4f117766dbb8428ed77e4fb82f378609a5c4
Albert Shadzhanov <albertm79@yandex.ru>
2025-04-14 11:36:54 +0300

    Adding one book to BooksCollector

---
+++ tests.py
@@ -0,0 +1,10 @@
+import pytest
+
+from main import BooksCollector
+
+
+class TestBooksCollector:
+    def test_add_new_book_add_one_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и мир')
+        assert len(collector.get_books_genre()) == 1
