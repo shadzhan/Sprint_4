344136b93d55164b1303dad36da9ecce7648527c
Albert Shadzhanov <albertm79@yandex.ru>
2025-04-14 15:29:23 +0300

    A test for adding books with long title

--- tests.py
+++ tests.py
@@ -14,3 +14,9 @@ class TestBooksCollector:
         collector.add_new_book('Война и мир')
         collector.add_new_book('Война и мир')
         assert len(collector.get_books_genre()) == 1
+
+    def test_add_new_book_add_books_long_title(self):
+        collector = BooksCollector()
+        long_title = 'Закат Российской империи или новые приключения неуловимых мстителей'
+        collector.add_new_book(long_title)
+        assert len(collector.get_books_genre()) == 0
